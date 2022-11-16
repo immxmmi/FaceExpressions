f906a9533c19832380766f0ce5563f9ed2b280c7
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-16 18:22:57 +0100

    Clean Code

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const video = document.getElementById('video')
-
+// Convert numbers in percent
 function roundNumber(number) {
     return Math.round((number * 100 + Number.EPSILON) * 100) / 100;
 }
 
+// Info about Customer Data that get collected von FACE-API
 class Customer {
     constructor(product, gender, genderProbability, age, angry, disgusted, fearful, happy, neutral, sad, surprise) {
         this.product = product;
@@ -21,6 +21,22 @@ class Customer {
 }
 
 
+// Global variable
+
+
+// Quality of Collected Data --> def. the limit of data --> take the avg. of the Data (higher the number more accurate calculation of Customer Data)
+const DATA_LENGTH = 50;
+// How fast calculate new Face Expressions
+const TIMER_REDRAW_FACE = 100;
+// List of all Customers
+const customerData = [];
+
+
+// VIDEO
+// Get the video Element to evaluate the Data
+const video = document.getElementById('video');
+
+// Function that start the video
 function startVideo() {
     navigator.mediaDevices.getUserMedia(
         {
@@ -34,7 +50,12 @@ function startVideo() {
     });
 }
 
+//IMG
+const image = document.getElementById('image_01');
 
+//FACE API
+
+// Load all Promise Data
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
@@ -44,7 +65,33 @@ Promise.all([
 ]).then(startVideo)
 
 
-const customerData = [];
+// Print the Data in the console
+function printCustomerData(customer) {
+    console.log(customerData);
+    console.log(" ------------------------------------ Expressions --------------------------------------");
+    console.log("Product: " + customer.product);
+    console.log("Gender: " + customer.gender + " zu " + customer.genderProbability + "%");
+    console.log("Age: " + customer.age);
+    console.log("Angry: " + customer.angry + "%");
+    console.log("Disgusted: " + customer.disgusted + "%");
+    console.log("Fearful: " + customer.fearful + "%");
+    console.log("Happy: " + customer.happy + "%");
+    console.log("Neutral: " + customer.neutral + "%");
+    console.log("Sad: " + customer.sad + "%");
+    console.log("Surprise: " + customer.surprise + "%");
+    console.log("---------------------------------------------------------------------------------------");
+}
+
+
+function drawFaceLandmarks(detections, displaySize) {
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+}
+
 
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
@@ -52,18 +99,20 @@ video.addEventListener('play', () => {
     const displaySize = {width: video.width, height: video.height}
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
-        //const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender();//.withFaceDescriptor()
-        const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+        const detections = await faceapi
+            .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender();
+
+        const age = [];
+        let counter = 0;
 
 
-        if (detections != null) {
+        if (detections !== null) {
             const customer = new Customer(
-                "Chips",
+                image.id,
                 detections.gender,
                 detections.genderProbability,
                 detections.age,
@@ -78,43 +127,29 @@ video.addEventListener('play', () => {
 
             customerData.push(customer);
 
-            console.log(customerData);
+            printCustomerData(customer);
 
-            console.log(" ------------------------------------ Expressions --------------------------------------");
-            console.log("Gender: " + customer.gender + " zu " + customer.genderProbability + "%");
-            console.log("Age: " + customer.age);
-            console.log("Angry: " + customer.angry + "%");
-            console.log("Disgusted: " + customer.disgusted + "%");
-            console.log("Fearful: " + customer.fearful + "%");
-            console.log("Happy: " + customer.happy + "%");
-            console.log("Neutral: " + customer.neutral + "%");
-            console.log("Sad: " + customer.sad + "%");
-            console.log("Surprise: " + customer.surprise + "%");
-            console.log("---------------------------------------------------------------------------------------");
 
-            // Every 1000 Data --> get evaluate
-            const data_length = 50;
-            const age = [];
-            if (customerData.length % data_length === 0) {
+            if (customerData.length % DATA_LENGTH === 0) {
                 customerData.forEach(createHistogram);
 
                 function createHistogram(item) {
-                    //console.log("TEST: ");
-                    //console.log(item.age);
                     age.push(item.age);
-                    console.log(age);
-
+                    age.forEach(function (item) {
+                        counter += item;
+                    })
                 }
             } else {
-                console.log("nicht genug daten");
+                console.log("not enough data");
             }
-
 
 
         }
 
-    }, 100)
+    }, TIMER_REDRAW_FACE)
 
 })
+
+
 startVideo()
 
